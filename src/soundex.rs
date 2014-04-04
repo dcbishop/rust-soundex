913404c334f05a3f78cf815affb4f9ca7747520d
David C. Bishop <david@davidbishop.org>
2014-04-04 21:23:08 +1100

    Add encode function.

--- src/soundex.rs
+++ src/soundex.rs
@@ -4,4 +4,8 @@ impl Soundex {
     pub fn new() -> Soundex {
         return Soundex;
     }
+
+    pub fn encode(&self, string: &str) -> ~str {
+        return string.clone().to_owned();
+    }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -4,6 +4,7 @@ mod soundex;
 
 #[allow(unused_variable)]
 #[test]
-fn soundex_encoding() {
+fn soundex_encoding_has_encode_function() {
     let soundex = Soundex::new();
+    let encoded = soundex.encode("A");
 }

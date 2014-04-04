0494f90178c0459cd6a6e3eb9a720bf812e8a736
David C. Bishop <david@davidbishop.org>
2014-04-04 22:47:43 +1100

    Refactoring.

--- src/soundex.rs
+++ src/soundex.rs
@@ -1,20 +1,34 @@
 pub struct Soundex;
 
+static MAX_CODE_LENGTH: uint = 4;
+
 impl Soundex {
     pub fn new() -> Soundex {
         return Soundex;
     }
 
     pub fn encode(&self, word: &str) -> ~str {
-        let mut encoded = word.slice_to(1).to_owned();
-        if word.len() > 1 {
-            encoded.push_char('1');
-        }
-        return zero_pad(encoded);
+        let encoded = zero_pad(head(word) + encoded_digits(word));
+        return encoded;
     }
 }
 
 fn zero_pad(string: &str) -> ~str {
-    let zeros_needed = 4 - string.len();
+    let zeros_needed = MAX_CODE_LENGTH - string.len();
     return string + "000".slice_to(zeros_needed);
+}
+
+fn head<'a>(string: &'a str) -> &'a str {
+    return string.slice_to(1);
+}
+
+fn encoded_digits(word: &str) -> ~str {
+    if word.len() > 1 {
+        return encode_digit();
+    }
+    return ~"";
+}
+
+fn encode_digit() -> ~str {
+    return ~"1";
 }
